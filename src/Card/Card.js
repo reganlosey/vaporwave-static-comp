5905e16f4391cd0215ec54e12f39d8c3652380f5
Regan Losey <regan.losey@gmail.com>
2022-02-24 18:22:56 -0700

    Add padding to link and change color of date text

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -2,21 +2,23 @@ import './Card.scss'
 
 
 const Card = ({ img }) => {
-  console.log(typeof img)
   return (
     <article className="card">
       <div className="card-header">
         <div className="card-header-txt">
-        <h1>Views</h1>
-        <p>02</p>
+          <h1>Views</h1>
+          <p>02</p>
         </div>
         <img className="card-header-img" src={img} />
       </div>
-      <div className="card-main-text">
-
-
+      <div className="card-main-txt">
+        <p>Recipient's Name Here</p>
+        <p>Monday, 01/01/1999 at 11:11 AM</p>
+        <p>
+          <a href="https://www.youtube.com/watch?v=-V9ku_9p3fo" target="_blank" rel="noreferrer" className="link">https://www.youtube.com/watch?v=-V9ku_9p3fo</a>
+        </p>
       </div>
-    </article>
+    </article >
   )
 
 
