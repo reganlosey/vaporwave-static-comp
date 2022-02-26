6d39b0022d3fc68af0bbdcf3dc47e88415eca10e
Regan Losey <regan.losey@gmail.com>
2022-02-25 18:26:44 -0700

    Add media query to stack header elements at 737px width

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -8,17 +8,18 @@ import shareArrow from '../assets/share_arrow.png'
 const Grid = () => {
   const imgCards = images.map((img) => {
     return (
-    <Card
-      key={Math.floor(Math.random() * Date.now())}
-      img={img}
-      arrow={shareArrow}
+      <Card
+        key={Math.floor(Math.random() * Date.now())}
+        img={img}
+        arrow={shareArrow}
       />
     )
   })
   return (
     <section className="grid-wrapper">
+      <p className="share-text">Shares</p>
       <div className="grid">
-      {imgCards}
+        {imgCards}
       </div>
     </section>
   )
